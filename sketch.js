5f5db745f00d77ad0f5da9559f7bee9bd660a2bc
kferjuste <kaylaferjuste@gmail.com>
2023-04-24 13:29:36 -0400

    attempt at linking through text

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
+let button1;
+let button2;
+let button3;
+let button4;
+
 function setup() {
   createCanvas(windowWidth, (windowHeight+200));
   //wanna make it scrollable, do windowWidth and then pick a height
+
 }
 
 function draw() {
@@ -19,7 +25,11 @@ function draw() {
   noStroke();
   fill('#1B4079');
   textAlign(LEFT);
-  text('unit 1 - machine learning TicTacToe', 40, 200);
+  unit1 = text('unit 1 - machine learning TicTacToe', 40, 200);
+  //button = createButton('unit 1 - machine learning TicTacToe');
+ // button.position(40,200);
+ // butonColor.setAlpha(button)
+  unit1.mouseClicked(window.open("unit1.html"))
 
   stroke('#4D7C8A');
   strokeWeight(4);
