b44aeaf71aa3af100db1d774c1f8230d6e309e6f
kferjuste <kaylaferjuste@gmail.com>
2023-05-08 23:11:26 -0400

    new website

--- sketch.js
+++ sketch.js
@@ -1,60 +1,30 @@
-let button1;
-let button2;
-let button3;
-let button4;
+var canvas;
 
 function setup() {
-  createCanvas(windowWidth, (windowHeight+200));
-  //wanna make it scrollable, do windowWidth and then pick a height
-
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
+  
 }
 
 function draw() {
-  background(250);
+  background(220);
+  frameRate(0.1);
+  
+  
+  const NUM_DOTS = 400;
+  
+  for (let i = 0; i < NUM_DOTS; i++) {
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() *window.innerHeight;
+    let a =  10 + Math.random() *5
+    let b = 10 + Math.random() *5
+    stroke(0.5);
+    ellipse(x, y, a, b)
+  }
+ 
+}
 
-  textSize(60);
-  textAlign(CENTER);
-
-  text('Kayla D Ferjuste', (window.innerWidth)/2, 100);
-
-  //figure out color scheme and start making it look pretty: colored stokes, background color, type shit
-  stroke('#1B4079');
-  strokeWeight(4);
-  rect(30, 170, (window.innerWidth - 60), 100, 20);
-  textSize(20);
-  noStroke();
-  fill('#1B4079');
-  textAlign(LEFT);
-  unit1 = text('unit 1 - machine learning TicTacToe', 40, 200);
-  //button = createButton('unit 1 - machine learning TicTacToe');
- // button.position(40,200);
- // butonColor.setAlpha(button)
-  unit1.mouseClicked(window.open("unit1.html"))
-
-  stroke('#4D7C8A');
-  strokeWeight(4);
-  rect(30, 320, (window.innerWidth - 60), 100, 20);
-  noStroke();
-  fill('#4D7C8A');
-  textAlign(LEFT);
-  text('unit 2 - 4-bit calculators', 40, 350);
-
-
-  stroke('#7F9C96');
-  strokeWeight(4);
-  rect(30, 470, (window.innerWidth - 60), 100, 20);
-  noStroke();
-  fill('#7F9C96');
-  textAlign(LEFT);
-  text('uni 3 - building trusses', 40, 500);
-
-  stroke('#8FAD88');
-  strokeWeight(4);
-  rect(30, 620, (window.innerWidth - 60), 100, 20);
-  noStroke();
-  fill('#8FAD88');
-  textAlign(LEFT);
-  text('unit 4 - personal project', 40, 650);
-
-  //another possible color 'CBDF90'
+function windowResized(){
+  resizeCanvas(window.innerWidth, window.innerHeight);
 }
